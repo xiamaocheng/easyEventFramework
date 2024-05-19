158b508cf90e9f82f9e3c9744ea51f473bdda1a4
xmc473@jsti.com <xmc473@jsti.com>
2024-05-19 14:41:47 +0800

    框架支持事件的(采集绑定处理分发）的基本完成

--- easyEventFramework/esayEvent/src/main/java/com/eventframe/demo/framework/main/EventFilter.java
+++ easyEventFramework/esayEvent/src/main/java/com/eventframe/demo/framework/main/EventFilter.java
@@ -1,13 +1,12 @@
 package com.eventframe.demo.framework.main;
 
 import com.eventframe.demo.framework.Event;
-import com.eventframe.demo.framework.filter.EventFilter;
 import com.eventframe.demo.framework.filter.MyEventFilter;
 
-public class EventFrameworkExample {
+public class EventFilter {
     public static void main(String[] args) {
         Event event = new Event(1, "data");
-        EventFilter filter = new MyEventFilter();
+        com.eventframe.demo.framework.filter.EventFilter filter = new MyEventFilter();
 
         if (filter.accept(event)) {
             System.out.println("Event accepted by filter");
